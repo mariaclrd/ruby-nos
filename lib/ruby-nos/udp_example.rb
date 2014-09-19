7418846db97febb26da0b0dae615885b22523a6f
mariaclrd <mariaclrd@gmail.com>
2014-09-19 17:33:30 +0100

    Learning how to use UDP with Ruby

---
+++ lib/ruby-nos/udp_example.rb
@@ -0,0 +1,8 @@
+require 'socket'
+
+s = UDPSocket.new
+s.bind(nil,1234)
+5.times do
+  text,sender = s.recvfrom(16)
+  puts test
+end
---
+++ lib/ruby-nos/udp_sender.rb
@@ -0,0 +1,14 @@
+require 'socket'
+require 'pry'
+
+class UDPSender
+  
+  def initialize
+    @socket = UDPSocket.new
+    @socket.bind(nil, 6600)
+  end
+  
+  def send_message message, host, port
+    @socket.send(message, 0, host, port)
+  end
+end
