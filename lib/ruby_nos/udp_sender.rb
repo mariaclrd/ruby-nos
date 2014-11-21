76490888836000d83675bcf1424e55ec7bd08e18
mariaclrd <mariaclrd@gmail.com>
2014-11-21 15:59:25 +0000

    Modified UDP sender to permit send to broadcast, configurable host and port or to last sender.

--- lib/ruby_nos/udp_sender.rb
+++ lib/ruby_nos/udp_sender.rb
@@ -5,19 +5,52 @@ module RubyNos
     include Initializable
     attr_accessor :socket, :port, :message, :receptor_address
 
-    def initialize
+    def initialize opts={}
       @socket = UDPSocket.new
-      @socket.bind("127.0.0.1", 0)
-      @port = Socket.getnameinfo(@socket.getsockname()).last
+      @socket.bind("127.0.0.1", opts[:port] || 0)
+      @socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
+      @port = port || Socket.getnameinfo(@socket.getsockname()).last
+    end
+
+    def receptor_address
+      @receptor_address ||= []
     end
 
     def send args={}
-      @socket.send(args[:message].to_s, 0, args[:host] || @receptor_address.last, args[:port] || @receptor_addres[1])
+      check_broadcast_host_receptor args[:host]
+      check_broadcast_port_receptor args[:port]
+      @socket.send(args[:message].to_s, 0, receptor_address[2], receptor_address[1])
     end
 
     def receive
       message, @receptor_address = @socket.recvfrom_nonblock(65535)
-      message
+      message_received message
+    end
+
+    def message_received message
+       message if message
+    end
+
+    private
+
+    def check_broadcast_host_receptor host
+      if host == :broadcast
+        receptor_address[2] = broadcast_address[0]
+      else
+        receptor_address[2] = host
+      end
+    end
+
+    def check_broadcast_port_receptor port
+      if port == :broadcast
+        receptor_address[1] = broadcast_address[1]
+      else
+        receptor_address[1] = port
+      end
+    end
+
+    def broadcast_address
+      ['255.255.255.255', 33333]
     end
   end
 end
