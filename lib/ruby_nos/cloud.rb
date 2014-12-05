f530bdf958de321986c99f8cf12e80a7aa902549
mariaclrd <mariaclrd@gmail.com>
2014-12-05 16:28:00 +0000

    Added new methods to keep the agents list in the cloud.

--- lib/ruby_nos/cloud.rb
+++ lib/ruby_nos/cloud.rb
@@ -1,16 +1,34 @@
-require "securerandom"
-
 module RubyNos
   class Cloud
     include Initializable
-    attr_accessor :uuid, :agents_list
+    attr_accessor :uuid, :agents_list, :agents_info
 
     def agents_list
       @agents_list ||= []
     end
 
+    def agents_info
+      @agents_info ||= []
+    end
+
     def add_agent uuid
       agents_list << uuid
+    end
+
+    def is_on_the_list? uuid
+      agents_list.include?(uuid)
+    end
+
+    def store_info message
+      agents_info << {:agent_uuid => message[:fr], :endpoints => message[:dt][:ep], :routes => message[:dt][:ru], :application => message[:dt][:ap]}
+    end
+
+    def find_for_agent_uuid uuid
+      agents_info.select{|e| e[:agent_uuid] == uuid}.first
+    end
+
+    def find_for_app app_name
+      agents_info.select{|e| e[:application] == app_name}.first
     end
   end
 end
--- spec/ruby_nos/cloud_spec.rb
+++ spec/ruby_nos/cloud_spec.rb
@@ -4,11 +4,43 @@ describe "#RubyNos::Cloud" do
   subject{Cloud.new(uuid:cloud_uuid)}
   let(:cloud_uuid) {"122445"}
 
-  describe "#join" do
+  describe "#add_agent" do
     let(:uuid) {"12345"}
     it "adds an agent to the agents_list" do
       subject.add_agent(uuid)
       expect(subject.agents_list.count).to eq(1)
+    end
+  end
+
+  describe "#store_info" do
+    let(:message) {Message.new({type: "PRS", data: {ap:"example_app"}}).serialize_with_optional_fields({:options => [:dt]})}
+    it "store the information of the message" do
+      subject.store_info(message)
+      expect(subject.agents_info.count).to eq(1)
+    end
+  end
+
+  describe "#find_for_app" do
+    let(:message) {Message.new({type: "PRS", from:"ag:12345", data: {ap:"example_app"}}).serialize_with_optional_fields({:options => [:dt]})}
+    it "finds the agent associated to a concrete application" do
+      subject.store_info(message)
+      expect(subject.find_for_app("example_app")[:agent_uuid]).to eq("ag:12345")
+    end
+  end
+
+  describe "#find_for_agent_uuid" do
+    let(:message) {Message.new({type: "PRS", from:"ag:12345", data: {ap:"example_app"}}).serialize_with_optional_fields({:options => [:dt]})}
+    it "finds the agent for its uuid" do
+      subject.store_info(message)
+      expect(subject.find_for_agent_uuid("ag:12345")[:agent_uuid]).to eq("ag:12345")
+    end
+  end
+
+  describe "#is_on_the_list?" do
+    let(:uuid) {"12345"}
+    it "says if an agent is on our local list or not" do
+      subject.add_agent(uuid)
+      expect(subject.is_on_the_list?(uuid)).to eq(true)
     end
   end
 end
