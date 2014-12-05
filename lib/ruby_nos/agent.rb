114a3e4d4e8868ee808efd404c0f684983c7aaba
mariaclrd <mariaclrd@gmail.com>
2014-12-05 16:26:58 +0000

    Created method configure and added udp_rx_socket.

--- lib/ruby_nos/agent.rb
+++ lib/ruby_nos/agent.rb
@@ -4,41 +4,46 @@ require "active_support"
 module RubyNos
   class Agent
     include Initializable
-    attr_accessor :uuid, :cloud, :udp_socket, :port, :cloud_uuid
+    attr_accessor :uuid, :cloud, :udp_tx_socket, :udp_rx_socket, :cloud_uuid, :processor
 
     def uuid
       @uuid ||= SecureRandom.uuid
     end
 
-    def udp_socket
-      @udp_socket ||= UDPSender.new
+    def udp_tx_socket
+      @udp_tx_socket ||= UDPSender.new
+    end
+
+    def udp_rx_socket
+      @udp_rx_socket ||= UDPReceptor.new
     end
 
     def cloud
       @cloud      ||= Cloud.new(uuid: cloud_uuid)
     end
 
-    def configure
-      join_cloud
-      check_health
+    def processor
+      @processor ||= Processor.new(self)
     end
 
-    def to_hash
-      {
-          agent_uuid: @uuid
-      }
+    def configure
+      listen
+      join_cloud
     end
 
     def send_message args={}
       message = Message.new({from: "ag:#{uuid}", to: args[:to] || "cd:#{cloud.uuid}", type: args[:type]})
-      udp_socket.send({host: args[:host], port: args[:port], message: message.serialize_message})
+      udp_tx_socket.send({host: args[:host], port: args[:port], message: message.serialize_message})
     end
 
     private
 
-    def join_cloud
-      send_message({type: 'DSC', host: :broadcast, port: :broadcast})
+    def listen
+      udp_rx_socket.listen(processor)
     end
 
+    def join_cloud
+      send_message({type: 'DSC'})
+    end
   end
 end
