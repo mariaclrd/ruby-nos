5cd417ff5e6a788fe3bc48af29e8163c29544760
mariaclrd <mariaclrd@gmail.com>
2015-01-23 16:20:09 +0000

    Added PRS message information.

--- lib/ruby_nos/agent.rb
+++ lib/ruby_nos/agent.rb
@@ -32,11 +32,24 @@ module RubyNos
     end
 
     def send_message args={}
-      message = Message.new({from: "ag:#{uuid}", to: args[:to] || "cd:#{cloud.uuid}", type: args[:type], sequence_number: args[:sequence_number]})
-      udp_tx_socket.send({host: args[:host], port: args[:port], message: message.serialize_message})
+      if args[:type] == "PRS"
+        data = receptor_info
+      end
+
+      if data
+        message = Message.new({from: "ag:#{uuid}", to: args[:to] || "cd:#{cloud.uuid}", type: args[:type], sequence_number: args[:sequence_number], data: data}).serialize_with_optional_fields({options: [:dt]})
+      else
+        message = Message.new({from: "ag:#{uuid}", to: args[:to] || "cd:#{cloud.uuid}", type: args[:type], sequence_number: args[:sequence_number]}).serialize_message
+      end
+
+      udp_tx_socket.send({host: args[:host], port: args[:port], message: message})
     end
 
     private
+
+    def receptor_info
+      {endpoints: ["UDP,#{udp_rx_socket.socket.connect_address.ip_port},#{udp_rx_socket.socket.connect_address.ip_address}"]}
+    end
 
     def listen
       udp_rx_socket.listen(processor)
--- spec/ruby_nos/agent_spec.rb
+++ spec/ruby_nos/agent_spec.rb
@@ -19,13 +19,15 @@ describe "#RubyNos::Agent" do
   describe "#send_message" do
     let(:cloud) {double("Cloud", :uuid => cloud_uuid)}
     let(:message){double("Message", :serialize_message => "SerializedMessage")}
-    let(:udp_socket){double("UDPSocket")}
+    let(:udp_socket){UDPReceptor.new(port)}
+    let(:well_formed_message){Message.new({from: "ag:#{subject.uuid}", to: "cd:#{subject.cloud.uuid}", type: "PRS", sequence_number: 3, data: {endpoints: ["UDP,#{udp_socket.socket.connect_address.ip_port},#{udp_socket.socket.connect_address.ip_address}"]}}).serialize_with_optional_fields({options: [:dt]})}
     let(:host) {"127.0.0.1"}
     let(:port) {"6700"}
 
     before(:each) do
       subject.cloud = cloud
     end
+
     it "sends a message using the UDP Socket" do
       expect(Message).to receive(:new).with({:from => "ag:#{subject.uuid}", :to => "cd:#{cloud.uuid}", :type => "DSC", :sequence_number => nil}).and_return(message)
       expect_any_instance_of(UDPSender).to receive(:send).with({host: host, port: port, :message => "SerializedMessage"})
@@ -36,6 +38,12 @@ describe "#RubyNos::Agent" do
       expect(Message).to receive(:new).with({:from => "ag:#{subject.uuid}", :to => "cd:#{cloud.uuid}", :type => "DSC", :sequence_number => 3}).and_return(message)
       expect_any_instance_of(UDPSender).to receive(:send).with({host: host, port: port, :message => "SerializedMessage"})
       subject.send_message({:type => "DSC", :port => port, :host => host, :sequence_number => 3})
+    end
+
+    it "add the UDP socket info if it is a presence message" do
+      allow(subject).to receive(:udp_rx_socket).and_return(udp_socket)
+      expect_any_instance_of(UDPSender).to receive(:send).with({host: host, port: port, :message => well_formed_message})
+      subject.send_message({:type => "PRS", :port => port, :host => host, :sequence_number => 3})
     end
   end
 end
