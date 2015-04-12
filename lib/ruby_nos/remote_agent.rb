a615655d206787f3de6c5fab5ce079a4fa56b55e
mariaclrd <mariaclrd@gmail.com>
2015-04-12 21:48:39 +0100

    Added comparison methods to remote agent.

--- lib/ruby_nos/remote_agent.rb
+++ lib/ruby_nos/remote_agent.rb
@@ -10,5 +10,21 @@ module RubyNos
     def add_endpoint *args
       endpoints << Endpoint.new(*args)
     end
+
+    def endpoints_collection
+      endpoints.map{|e| e.to_hash}
+    end
+
+    def same_timestamp? another_agent
+      timestamp == another_agent.timestamp
+    end
+
+    def same_endpoints? another_agent
+      endpoints_collection == another_agent.endpoints_collection
+    end
+
+    def same_api? another_agent
+      rest_api.to_hash == another_agent.rest_api.to_hash
+    end
   end
 end
--- spec/ruby_nos/remote_agent_spec.rb
+++ spec/ruby_nos/remote_agent_spec.rb
@@ -1,12 +1,46 @@
 require "spec_helper"
 
 describe "#RubyNos::RemoteAgent" do
-  subject{RemoteAgent.new}
+  subject{RemoteAgent.new({timestamp: 1, endpoints:[Endpoint.new({type: "UDP"})], rest_api: RestApi.new})}
+  let(:another_agent){RemoteAgent.new({timestamp: 2, endpoints: [Endpoint.new({type: "HTTP"})], rest_api: RestApi.new})}
+
+  before(:each) do
+    subject.rest_api.add_endpoint({type: "PUBLIC"})
+    another_agent.rest_api.add_endpoint({type: "HEALTHCHECK"})
+  end
 
   describe "#add_endpoint" do
     it "adds and endpoint to the list of endpoints for the remote agent" do
       subject.add_endpoint({type: "UDP"})
-      expect(subject.endpoints.count).to eq(1)
+      expect(subject.endpoints.count).to eq(2)
+    end
+  end
+
+  describe "#endpoints collection" do
+    it "shows the endpoints as an array of hashes" do
+      subject.add_endpoint({type: "UDP"})
+      expect(subject.endpoints_collection.first.keys).to eq([:path, :port, :sticky, :type, :priority, :host])
+    end
+  end
+
+  describe "#same_timestamp?" do
+    it "compares the timestamp with another agent" do
+      expect(subject.same_timestamp?(subject)).to eq(true)
+      expect(subject.same_timestamp?(another_agent)).to eq(false)
+    end
+  end
+
+  describe "#same_endpoints?" do
+    it "compares the timestamp with another agent" do
+      expect(subject.same_endpoints?(subject)).to eq(true)
+      expect(subject.same_endpoints?(another_agent)).to eq(false)
+    end
+  end
+
+  describe "#same_api?" do
+    it "compares the timestamp with another agent" do
+      expect(subject.same_api?(subject)).to eq(true)
+      expect(subject.same_api?(another_agent)).to eq(false)
     end
   end
 end
